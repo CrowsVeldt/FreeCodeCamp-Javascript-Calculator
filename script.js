8128c3468f78d8f30d4b117f4217c4d9c3906303
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-07 09:56:13 +0300

    my computer is working again, back to work. added title and text box to html, comment with next step to js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 (function () {
-  "use strict";
-  
-  
-}());
+  'use strict'
+    
+    /* Okay, so I need to relearn how to get text from the text box (I think it should be with getElement or the like). MAKE SURE TO SANITIZE THE CONTENTS BEFORE ACCEPTING THEM! Only allow: Numbers, and the math operators that will be on the calculator.
+    
+    then I need to use that text to perform mathematical calculations (try eval at first, after search for more acceptable ways of doing it)
+    
+    then I need to populate the text box with the results
+    */
+    
+}())
