d3cc2ccbf919599df714d6f4807feb5f71cc1b31
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-07 19:35:45 +0300

    added comments to remind me what to write next

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 (function () {
   'use strict'
-    
-    /* Okay, so I need to relearn how to get text from the text box (I think it should be with getElement or the like). MAKE SURE TO SANITIZE THE CONTENTS BEFORE ACCEPTING THEM! Only allow: Numbers, and the math operators that will be on the calculator.
-    
-    then I need to use that text to perform mathematical calculations (try eval at first, after search for more acceptable ways of doing it)
-    
-    then I need to populate the text box with the results
-    */
-    
+
+    // object valueStore
+
+    // function readInput
+
+    // function evaluateInput
+
+    // function printOutput
 }())
+
+/* Okay, so I need to relearn how to get text from the text box (I think it should be with getElement or the like). MAKE SURE TO SANITIZE THE CONTENTS BEFORE ACCEPTING THEM! Only allow: Numbers, and the math operators that will be on the calculator.
+
+then I need to use that text to perform mathematical calculations (try eval at first, after search for more acceptable ways of doing it)
+
+then I need to populate the text box with the results
+*/
