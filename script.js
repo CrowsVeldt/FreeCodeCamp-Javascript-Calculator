dc54a670c10e184e6fea91d33da8167949a1b0b6
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-08 10:15:21 +0300

    realized I was going at it in the wrong way. Explanation in JS comment. Added sections to HTML to display results and hold buttons, changed all double-quotes to single quotes

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
+//I've been going about this the wrong way, I thought it would be simple to use a text box, but clearly that was ass-backwards. I'm going to make buttons for numbers and functions, a section to display the vales to the user, and an equals button to print the result
+
+
 (function () {
   'use strict'
 
     // array valueStore
   const valueStore = []
-
-    // function readInput
-  function readInput (){
-      
-      //TODO: When the user hits 'return' or clicks the '=' button, read the contents of the text box and print them to console
-      
-      
-  }
-
-    // function evaluateInput
-
-    // function printOutput
 }())
-
-/* Okay, so I need to to get text from the text box, MAKE SURE TO SANITIZE THE CONTENTS BEFORE ACCEPTING THEM! Only allow: Numbers, and the math operators that will be on the calculator.
-
-then I need to use that text to perform mathematical calculations
-
-then I need to populate the text box with the results
-*/
