584f5f378f66038652acac5e960c015e7bfe2ec6
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-10 12:53:18 +0300

    added all number buttons, changed valueStore to a string

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 (function () {
   'use strict'
-
-    //Option: make valueStore a string and use eval on it in the end
-    // array valueStore
-  const valueStore = []
+    
+  // string to store the values from user input
+  const valueStore = ''
   
   // add eventListener to buttons class normalButton, when pressed add they're value to value store, and display it in displaySection
   
   // add eventListener to equalsButton when pressed perform evaluation and display result
-  
-  
 }())
