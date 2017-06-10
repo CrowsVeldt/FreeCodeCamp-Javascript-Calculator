4a3e3bb7a511f6afd9f87db4de9dd60edc273d15
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-10 08:29:43 +0300

    added more buttons, added classes to all buttons, updated comments with plan

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//I've been going about this the wrong way, I thought it would be simple to use a text box, but clearly that was ass-backwards. I'm going to make buttons for numbers and functions, a section to display the vales to the user, and an equals button to print the result
-
-
 (function () {
   'use strict'
 
+    //Option: make valueStore a string and use eval on it in the end
     // array valueStore
   const valueStore = []
+  
+  // add eventListener to buttons class normalButton, when pressed add they're value to value store, and display it in displaySection
+  
+  // add eventListener to equalsButton when pressed perform evaluation and display result
+  
+  
 }())
