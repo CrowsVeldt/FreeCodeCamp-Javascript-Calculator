2168ac15f7cb02489ae62e7ce59993e8adf7b88e
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-10 13:44:07 +0300

    added event listener to all normalButtons, adds the buttons value to valueStore when pressed

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
   'use strict'
     
   // string to store the values from user input
-  const valueStore = ''
+  var valueStore = ''
   
   const normalButtons = document.querySelectorAll('.normalButton')
   
-  // use forEach to add eventListener to all of normalButtons, when pressed add they're value to value store, and display it in displaySection
+  // add eventListener to all of normalButtons
+  normalButtons.forEach(function(item){
+    item.addEventListener('click', function(){
+      
+      // on click add button value to valueStore
+      valueStore += item.innerHTML
+        
+      // and display it in displaySection
+        
+    })
+  })
+  
   
   // add eventListener to equalsButton when pressed perform evaluation and display result
 }())
