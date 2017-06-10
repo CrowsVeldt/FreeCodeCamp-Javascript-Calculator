2af85cb086b5ab0e934a9eeadf7e85ddb98658ba
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-10 12:59:36 +0300

    added const with querySelectorAll for all .normalButtons

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
   // string to store the values from user input
   const valueStore = ''
   
-  // add eventListener to buttons class normalButton, when pressed add they're value to value store, and display it in displaySection
+  const normalButtons = document.querySelectorAll('.normalButton')
+  
+  // use forEach to add eventListener to all of normalButtons, when pressed add they're value to value store, and display it in displaySection
   
   // add eventListener to equalsButton when pressed perform evaluation and display result
 }())
