417f26dea090e2bd135ae84dc753561fed7240b1
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-11 19:16:30 +0300

    cleaned up JS file, tried to add icon to README. We'll see how that works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A simple Javascript calculator for the FreeCodeCamp Front End track. 
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
--- script.js
+++ script.js
@@ -1,49 +1,41 @@
 (function () {
   'use strict'
-    
+
   // string to store the values from user input
   var valueStore = ''
-  
+
   // add const to access display section
   const display = document.getElementById('displaySection')
-  
+
   // add const to access Buttons
   const buttons = document.querySelectorAll('button')
-  
+
   // add eventListener to all of normalButtons
-  buttons.forEach(function(item){
-    item.addEventListener('click', function(){
+  buttons.forEach(function (item) {
+    item.addEventListener('click', function () {
       // if 'item' is a number add it to valueStore and display valueStore
-      if(item.classList.contains('numberButton')){
-         
+      if (item.classList.contains('numberButton')) {
         // on click add button value to valueStore
         valueStore += item.innerHTML
-
         // and display it in displaySection
         display.innerHTML = valueStore
-      
       // if 'item' is an operator add it to valueStore and display valueStore
-      } else if(item.classList.contains('operatorButton')){
-        
+      } else if (item.classList.contains('operatorButton')) {
         // on click add button value to valueStore
         valueStore += item.innerHTML
-
         // and display it in displaySection
         display.innerHTML = valueStore
-      
-      //if 'item' is clear
-      } else if(item.classList.contains('clearButton')){
-        //set ValueStore to 0
+      // if 'item' is clear
+      } else if (item.classList.contains('clearButton')) {
+        // set ValueStore to 0
         valueStore = '0'
-        //empty display
+        // empty display
         display.innerHTML = null
       // if 'item' is equals
-      } else if(item.classList.contains('equalsButton'))  {
+      } else if (item.classList.contains('equalsButton')) {
         // evaluate valueStore and display the result
         display.innerHTML = eval(valueStore)
       }
-      
     })
   })
-
 }())
