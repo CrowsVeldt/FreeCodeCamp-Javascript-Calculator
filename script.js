dca7fa2c737a9dc5a97ceacb720665fbc884f2cc
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-12 09:20:36 +0300

    split display section into "total" and "history" sections, updated JS to reflect this

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
   var valueStore = ''
 
   // add const to access display section
-  const display = document.getElementById('displaySection')
+  const history = document.getElementsByClassName('history')
+  
+  const total = document.getElementsByClassName('total')
 
   // add const to access Buttons
   const buttons = document.querySelectorAll('button')
@@ -18,23 +20,27 @@
         // on click add button value to valueStore
         valueStore += item.innerHTML
         // and display it in displaySection
-        display.innerHTML = valueStore
+        history[0].innerHTML = valueStore
       // if 'item' is an operator add it to valueStore and display valueStore
       } else if (item.classList.contains('operatorButton')) {
+        
         // on click add button value to valueStore
         valueStore += item.innerHTML
         // and display it in displaySection
-        display.innerHTML = valueStore
+        history[0].innerHTML = valueStore
       // if 'item' is clear
+        
       } else if (item.classList.contains('clearButton')) {
         // set ValueStore to 0
         valueStore = '0'
-        // empty display
-        display.innerHTML = null
+        // empty history
+        history[0].innerHTML = null
+        // empty total
+        total[0].innerHTML = null
       // if 'item' is equals
       } else if (item.classList.contains('equalsButton')) {
         // evaluate valueStore and display the result
-        display.innerHTML = eval(valueStore)
+        total[0].innerHTML = eval(valueStore)
       }
     })
   })
