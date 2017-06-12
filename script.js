c9a0acc898b4bea5eb00721506b70695fe357a71
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-12 10:22:27 +0300

    changed total section and total const in preperation for major refactoring

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 
   // add const to access display section
   const history = document.getElementsByClassName('history')
-  
-  const total = document.getElementsByClassName('total')
+
+  const totalOrCurrent = document.getElementsByClassName('total-current')
 
   // add const to access Buttons
   const buttons = document.querySelectorAll('button')
@@ -23,24 +23,22 @@
         history[0].innerHTML = valueStore
       // if 'item' is an operator add it to valueStore and display valueStore
       } else if (item.classList.contains('operatorButton')) {
-        
         // on click add button value to valueStore
         valueStore += item.innerHTML
         // and display it in displaySection
         history[0].innerHTML = valueStore
       // if 'item' is clear
-        
       } else if (item.classList.contains('clearButton')) {
         // set ValueStore to 0
         valueStore = '0'
         // empty history
         history[0].innerHTML = null
         // empty total
-        total[0].innerHTML = null
+        totalOrCurrent[0].innerHTML = null
       // if 'item' is equals
       } else if (item.classList.contains('equalsButton')) {
         // evaluate valueStore and display the result
-        total[0].innerHTML = eval(valueStore)
+        totalOrCurrent[0].innerHTML = eval(valueStore)
       }
     })
   })
