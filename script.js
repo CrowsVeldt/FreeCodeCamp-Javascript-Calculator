646ead297f987b9a82cd0183998e2295fa93e9e1
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-15 10:40:33 +0300

    added functionality to equals button, cleaned up a bit, updated display to show only the last entry, undid previous commit. I need to work on making smaller commits

--- script.js
+++ script.js
@@ -2,162 +2,117 @@
   'use strict'
 
   var history = []
- 
-  var argument1 = ''
-  
-  var argument2 = ''
+
+  var arg1 = ''
+
+  var arg2 = ''
 
   var currentOperator = ''
-  
+
   // display stuff on the screen
   function display () {
-    
-  const logDisplay = document.getElementsByClassName ('log-display')
-  const currentDisplay = document.getElementsByClassName ('current-display')
-  
-  currentDisplay[0].innerHTML = argument1 + ' ' + currentOperator + ' ' + argument2
-  logDisplay[0].innerHTML = history
-  
-  }
-  
-  
-  // recieve and clean input
-  function input () {
-    
-    const buttons = document.querySelectorAll ('button')
-    
-    buttons.forEach (function(input) {
-      input.addEventListener('click', function(){
-        
-        
-        if (currentOperator === '') { // if currentOperator is empty
-          
-          if (input.classList.contains('number')) {
-            
-            argument1 += input.innerHTML
-            
-            display ()
-            
-          } else if (input.classList.contains('decimal')) {
-            
-            // fill stuff here
-          
-          } else if (input.classList.contains('operator')) {
-            
-            currentOperator = input.innerHTML
-            
-            display ()
-            
-          } else if (input.classList.contains('equals')) {
-            
-            // fill stuff here
-            
-          }
-          
-          
-        } else { // if currentOperator is not empty
-        
-          if (input.classList.contains('number')) {
-            
-            argument2 += input.innerHTML
-            
-            display ()
-            
-          } else if (input.classList.contains('decimal')) {
-            
-            // fill stuff here
-          
-          } else if (input.classList.contains('operator')) {
-            
-            currentOperator = input.innerHTML
-            
-            display ()
-            
-          } else if (input.classList.contains('clear')) {
-            
-            history = ''
-            argument1 = ''
-            argument2 = ''
-            currentOperator = ''
-            display ()
-            
-          } else if (input.classList.contains('clear-current')) {
+    const logDisplay = document.getElementsByClassName('log-display')
+    const currentDisplay = document.getElementsByClassName('current-display')
 
-            argument1 = ''
-            argument2 = ''
-            currentOperator = ''
-            display ()
-            
-          } else if (input.classList.contains('equals')) {
-            
-            // fill stuff here
-            
-          }
-         
-        }
-        
-        
-        
-        
-//        if (input.classList.contains('number')) {
-//          
-//          if (currentOperator === '') {
-//            
-//            argument1 += input.innerHTML
-//          
-//            display ()
-//            
-//          } else {
-//            
-//            argument2 += input.innerHTML
-//          
-//            display ()
-//            
-//          }
-          
-//        } else if (input.classList.contains('decimal')) {
-//          
-//          if ()
-//          
-//          current += input.innerHTML
-//          
-//          display ()
-//          
-//        } else if (input.classList.contains('operator')) {
-//          
-//          current += input.innerHTML
-//          
-//          display ()
-//          
-//        } else if (input.classList.contains('clear')) {
-//          
-//          history = ''
-//          
-//          current = ''
-//          
-//          display ()
-//          
-//        } else if (input.classList.contains('clear-current')) {
-//          
-//          current = ''
-//          
-//          display ()
-//          
-//        }
-        
-      })
-      
-    })
-    
+    currentDisplay[0].innerHTML = arg1 + ' ' + currentOperator + ' ' + arg2
+    logDisplay[0].innerHTML = history[history.length - 1]
   }
-  
-  input ()
-  
-  
+
   // evaluate stuff, seperated out for general good code-liness and in case I want to mess with the evaluation function in the future
   function evaluate (stuff) {
-    
     return eval(stuff)
-    
   }
+
+  // recieve and clean input
+  function input () {
+    const buttons = document.querySelectorAll('button')
+
+    buttons.forEach(function (input) {
+      input.addEventListener('click', function () {
+        if (currentOperator === '') { // if currentOperator is empty
+          if (input.classList.contains('number')) {
+            arg1 += input.innerHTML
+
+            display()
+          } else if (input.classList.contains('decimal')) {
+
+            // fill stuff here
+
+          } else if (input.classList.contains('operator')) {
+            currentOperator = input.innerHTML
+
+            display()
+          } else if (input.classList.contains('clear')) {
+            history = []
+
+            arg1 = ''
+
+            arg2 = ''
+
+            currentOperator = ''
+
+            display()
+          } else if (input.classList.contains('clear-current')) {
+            arg1 = ''
+
+            arg2 = ''
+
+            currentOperator = ''
+
+            display()
+          } else if (input.classList.contains('equals')) {
+            history.push([arg1, currentOperator, arg2])
+
+            display()
+          }
+        } else { // if currentOperator is not empty
+          if (input.classList.contains('number')) {
+            arg2 += input.innerHTML
+
+            display()
+          } else if (input.classList.contains('decimal')) {
+
+            // fill stuff here
+
+          } else if (input.classList.contains('operator')) {
+            currentOperator = input.innerHTML
+
+            display()
+          } else if (input.classList.contains('clear')) {
+            history = []
+
+            arg1 = ''
+
+            arg2 = ''
+
+            currentOperator = ''
+
+            display()
+          } else if (input.classList.contains('clear-current')) {
+            arg1 = ''
+
+            arg2 = ''
+
+            currentOperator = ''
+
+            display()
+          } else if (input.classList.contains('equals')) {
+            history.push([arg1, currentOperator, arg2])
+
+            let current = arg1 + currentOperator + arg2
+
+            arg1 = evaluate(current)
+
+            arg2 = ''
+
+            currentOperator = ''
+
+            display()
+          }
+        }
+      })
+    })
+  }
+
+  input()
 }())
