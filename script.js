c2e67c50ea68bac4e6848a3f5595706a4020c5ce
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-06-17 10:41:39 +0300

    started work on issue #2, added equationToEvaluate to js, added temp dislpay for testing to HTML

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 (function () {
   'use strict'
-
+  
+  // the history of all entries
   var history = []
-
+  
+  // the equation that will be evaluated after pressing 'equals' 
+  var equationToEvaluate = []
+  
+  // the current number 
   var currentEntry = ''
   
+  // the last character of the current number
   var lastCharacter = ''
 
-  // is the currentEntry number already a decimal/contains a decimal?
+  // does currentEntry already contain a decimal?
   var decimalPlaced = false
 
-  // display entry on the screen
+  // display on the screen
   function display () {
-    const logDisplay = document.getElementsByClassName('log-display')
-    const currentDisplay = document.getElementsByClassName('current-display')
+    const logDisplay = document.getElementsByClassName('log')
+    const currentDisplay = document.getElementsByClassName('current')
+    
+    const tempDisplay = document.getElementsByClassName('temp')
 
     currentDisplay[0].innerHTML = currentEntry
     logDisplay[0].innerHTML = history
+    tempDisplay[0].innerHTML = equationToEvaluate
   }
 
   // evaluate the function to display
-  function evaluate (entry) {
-    // clean leading zeroes from 'entry'
-    while (entry.charAt(0) === '0' && entry.charAt(1) !== '.') {
-      entry = entry.slice(1)
+  function evaluate (currentEntry) {
+    // clean leading zeroes from 'currentEntry'
+    while (currentEntry.charAt(0) === '0' && currentEntry.charAt(1) !== '.') {
+      currentEntry = currentEntry.slice(1)
     }
 
-    return eval(entry).toString()
+    return eval(currentEntry).toString()
   }
 
   // recieve and clean input
@@ -37,13 +46,17 @@
       input.addEventListener('click', function () {
         if (input.classList.contains('clear')) {
           history = []
+          
+          equationToEvaluate = []
 
           currentEntry = ''
 
           decimalPlaced = false
 
           display()
-        } else if (input.classList.contains('clear-currentEntry')) {
+        } else if (input.classList.contains('clear-current')) {
+          equationToEvaluate = []
+          
           currentEntry = ''
           
           decimalPlaced = false
@@ -61,20 +74,23 @@
           decimalPlaced = true
         } else if (input.classList.contains('operator')) {
           if (currentEntry !== '' && lastCharacter !== '+' && lastCharacter !== '-' && lastCharacter !== '/' && lastCharacter !== '*') {
+            // add the operator to currentEntry
             currentEntry += input.innerHTML
+            // then push currentEntry to equationToEvaluate
+            equationToEvaluate.push([currentEntry])
 
             display()
 
             decimalPlaced = false
           }
         } else if (input.classList.contains('equals')) {
-          // if currentEntry is not empty and the last and first char are not an operators
-          if (currentEntry !== '' && lastCharacter !== '+' && lastCharacter !== '-' && lastCharacter !== '/' && lastCharacter !== '*') {
+          // if currentEntry is not empty
+          if (currentEntry !== '') {
             // (re)set decimalPlaced
             decimalPlaced = false
             // push currentEntry to history
             history.push(currentEntry)
-            // return the evaluation of currentEntry as a string (to allow delete to work)
+
             currentEntry = evaluate(currentEntry)
 
             display()
@@ -82,7 +98,7 @@
         } else if (input.classList.contains('previous')) {
           // if history is not empty
           if (history.length > 0) {
-            // return previous entry
+            // return previous currentEntry
             currentEntry = history.pop()
 
             display()
